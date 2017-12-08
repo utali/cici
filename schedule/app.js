a8b579528adf971dfba596d12b0fff4151cd4689
liqiaoqiao <liqiaoqiao@musicq.local>
2017-12-08 14:11:29 +0800

    nodeStart

--- schedule/app.js
+++ schedule/app.js
@@ -12,9 +12,32 @@ var schedule = require("node-schedule");
 // });
 
 //每小时的固定分钟执行任务 ex:05分
-var rule = new schedule.RecurrenceRule();
-rule.minute = 5;
+// var rule = new schedule.RecurrenceRule();
+// rule.minute = 5;
+//
+// var task = schedule.scheduleJob(rule, function () {
+//     console.log('05分执行任务');
+// });
 
+//一周中的某些天，某个时刻 ex:每天的14:10
+// var rule = new schedule.RecurrenceRule();
+// rule.dayOfWeek = [0,new schedule.Range(0,6)];
+// rule.hour = 14;
+// rule.minute = 10;
+//
+// var task = schedule.scheduleJob(rule, function () {
+//     console.log('每天10分执行任务');
+// });
+
+//每隔10秒执行
+var rule = new schedule.RecurrenceRule();
+var times = [];
+for (var i=1; i<60; i+=10){
+    times.push(i);
+}
+rule.second = times;
+var c = 0;
 var task = schedule.scheduleJob(rule, function () {
-    console.log('05分执行任务');
+    c+=10;
+    console.log(c);
 });
