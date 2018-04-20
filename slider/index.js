c8988c3fc0f5c9a3b235acd6630bca6abbd79e61
liqiaoqiao <liqiaoqiao@musicq.local>
2018-04-20 10:28:01 +0800

    滑块验证

--- slider/index.js
+++ slider/index.js
@@ -1,106 +1,292 @@
 /**
- * Created by liqiaoqiao on 2018/4/17.
+ * Created by liqiaoqiao on 2018/4/19.
  */
-(function ($) {
-    $.fn.imgcode = function (options) {
-        //初始化参数
-        var defaults = {
-            callback:""  //回调函数
-        };
-        var opts = $.extend(defaults, options);
-        return this.each(function () {
-            var $this = $(this);//获取当前对象
-            var html = '<div class="code-k-div">' +
-                '<div class="code_bg"></div>' +
-                '<div class="code-con">' +
-                '<div class="code-img">' +
-                '<div class="code-img-con">' +
-                '<div class="code-mask"><img src="./images/ai.png"></div>' +
-                '<img src="./images/hotnews.jpg""></div>' +
-                '<div class="code-push"><i class="icon-login-bg icon-w-25 icon-push">刷新</i><span class="code-tip"></span></div>' +
-                '</div>' +
-                '<div class="code-btn">' +
-                '<div class="code-btn-img code-btn-m"></div>' +
-                '<span>按住滑块，拖动完成上方拼图</span>' +
-                '</div></div></div>';
-            $this.html(html);
 
-            //定义拖动参数
-            var $divMove = $(this).find(".code-btn-img"); //拖动按钮
-            var $divWrap = $(this).find(".code-btn");//鼠标可拖拽区域
-            var mX = 0, mY = 0;//定义鼠标X轴Y轴
-            var dX = 0, dY = 0;//定义滑动区域左、上位置
-            var isDown = false;//mousedown标记
-            if(document.attachEvent) {//ie的事件监听，拖拽div时禁止选中内容，firefox与chrome已在css中设置过-moz-user-select: none; -webkit-user-select: none;
-                $divMove[0].attachEvent('onselectstart', function() {
-                    return false;
-                });
-            }
-            //按钮拖动事件
-            $divMove.on({
-                mousedown: function (e) {
-                    //清除提示信息
-                    $this.find(".code-tip").html("");
-                    var event = e || window.event;
-                    mX = event.pageX;
-                    dX = $divWrap.offset().left;
-                    dY = $divWrap.offset().top;
-                    isDown = true;//鼠标拖拽启
-                    $(this).addClass("active");
-                    //修改按钮阴影
-                    $divMove.css({"box-shadow":"0 0 8px #666"});
-                }
-            });
-            //鼠标点击松手事件
-            $(document).mouseup(function (e) {
-                var lastX = $this.find(".code-mask").offset().left - dX - 1;
-                isDown = false;//鼠标拖拽启
-                $divMove.removeClass("active");
-                //还原按钮阴影
-                $divMove.css({"box-shadow":"0 0 3px #ccc"});
-                checkcode(lastX);
-            });
-            //滑动事件
-            $divWrap.mousemove(function (event) {
-                var event = event || window.event;
-                var x = event.pageX;//鼠标滑动时的X轴
-                if (isDown) {
-                    if(x>(dX+30) && x<dX+$(this).width()-20){
-                        $divMove.css({"left": (x - dX - 20) + "px"});//div动态位置赋值
-                        $this.find(".code-mask").css({"left": (x - dX-30) + "px"});
-                    }
-                }
-            });
-            //验证数据
-            function checkcode(code){
-                var iscur=false;
-                //模拟ajax
-                setTimeout(function(){
-                    if(iscur){
-                        checkcoderesult(1,"验证通过");
-                        $this.find(".code-k-div").hide();
-                        opts.callback({code:1000,msg:"验证通过",msgcode:"23dfdf123"});
-                    }else{
-                        $divMove.addClass("error");
-                        checkcoderesult(0,"验证不通过");
-                        opts.callback({code:1001,msg:"验证不通过"});
-                        setTimeout(function() {
-                            $divMove.removeClass("error");
-                            $this.find(".code-mask").animate({"left":"0px"},200);
-                            $divMove.animate({"left": "10px"},200);
-                        },400);
-                    }
-                },500)
-            }
-            //验证结果
-            function checkcoderesult(i,txt){
-                if(i==0){
-                    $this.find(".code-tip").addClass("code-tip-red");
-                }else{
-                    $this.find(".code-tip").addClass("code-tip-green");
-                }
-                $this.find(".code-tip").html(txt);
-            }
-        })
+$(document).ready(function() {
+    $.ajaxSetup({
+        async : false
+    });
+    //随机生成拼图块
+    function strockArc(ctx,mX,n){//圆心(x,y)，半径radius
+        var number = Math.floor(Math.random()*9)%9;
+        if ( n>=0 ) number = n;
+        switch (number) {
+            case 0:
+                //上凸下凹
+                ctx.arc(mX+s/2,cY,r,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX+s/2+r,cY+s);
+                ctx.arc(mX+s/2,cY+s,r,2*Math.PI,Math.PI,true);
+                ctx.lineTo(mX,cY+s);
+                return number;
+                break;
+            case 1:
+                //上凸右凹
+                ctx.arc(mX+s/2,cY,r,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s/2-r);
+                ctx.arc(mX+s,cY+s/2,r,1.5*Math.PI,0.5*Math.PI,true);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX,cY+s);
+                return number;
+                break;
+            case 2 :
+                //上凸左凹
+                ctx.arc(mX+s/2,cY,25,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX,cY+s);
+                ctx.lineTo(mX,cY+s/2+r);
+                ctx.arc(mX,cY+s/2,25,0.5*Math.PI,1.5*Math.PI,true);
+                return number;
+                break;
+            case 3 :
+                //上凸下凸
+                ctx.arc(mX+s/2,cY,r,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX+s/2+r,cY+s);
+                ctx.arc(mX+s/2,cY+s,r,0,Math.PI);
+                ctx.lineTo(mX,cY+s);
+                return number;
+                break;
+            case 4 :
+                //上凸右凸
+                ctx.arc(mX+s/2,cY,r,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s/2-r);
+                ctx.arc(mX+s,cY+s/2,r,1.5*Math.PI,0.5*Math.PI);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX,cY+s);
+                return number;
+                break;
+//                case 5 :
+//                    //上凸左凸
+//                    ctx.arc(mX+s/2,cY,25,Math.PI,2*Math.PI);
+//                    ctx.lineTo(mX+s,cY);
+//                    ctx.lineTo(mX+s,cY+s);
+//                    ctx.lineTo(mX,cY+s);
+//                    ctx.lineTo(mX,cY+s/2+r);
+//                    ctx.arc(mX,cY+s/2,25,0.5*Math.PI,1.5*Math.PI);
+//                    return number;
+//                    break;
+            case 5 :
+                //上凸右凸下凹
+                ctx.arc(mX+s/2,cY,r,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s/2-r);
+                ctx.arc(mX+s,cY+s/2,r,1.5*Math.PI,0.5*Math.PI);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX+s/2+r,cY+s);
+                ctx.arc(mX+s/2,cY+s,r,2*Math.PI,Math.PI,true);
+                ctx.lineTo(mX,cY+s);
+                return number;
+                break;
+            case 6 :
+                //上凸右凸左凹
+                ctx.arc(mX+s/2,cY,r,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s/2-r);
+                ctx.arc(mX+s,cY+s/2,r,1.5*Math.PI,0.5*Math.PI);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX,cY+s);
+                ctx.lineTo(mX,cY+s/2+r);
+                ctx.arc(mX,cY+s/2,25,0.5*Math.PI,1.5*Math.PI,true);
+                return number;
+                break;
+            case 7 :
+                //上凸下凸右凹
+                ctx.arc(mX+s/2,cY,r,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s/2-r);
+                ctx.arc(mX+s,cY+s/2,r,1.5*Math.PI,0.5*Math.PI,true);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX+s/2+r,cY+s);
+                ctx.arc(mX+s/2,cY+s,r,0,Math.PI);
+                ctx.lineTo(mX,cY+s);
+                return number;
+                break;
+            case 8 :
+                //上凸左凹右凹
+                ctx.arc(mX+s/2,cY,r,Math.PI,2*Math.PI);
+                ctx.lineTo(mX+s,cY);
+                ctx.lineTo(mX+s,cY+s/2-r);
+                ctx.arc(mX+s,cY+s/2,r,1.5*Math.PI,0.5*Math.PI,true);
+                ctx.lineTo(mX+s,cY+s);
+                ctx.lineTo(mX,cY+s);
+                ctx.lineTo(mX,cY+s/2+r);
+                ctx.arc(mX,cY+s/2,25,0.5*Math.PI,1.5*Math.PI,true);
+                return number;
+                break;
+        }
     }
-})(jQuery);
+
+    //拼图随机生成位置
+    var r = 25,s=150;
+    var n, resultData;
+    var cX, cY;
+    var imgData;
+    var isMatch = false;
+
+    var canvas_img = document.getElementById('yan_img2');
+    var ctx_img = canvas_img.getContext('2d');
+
+    // var img = document.getElementById('yan_img');
+    // ctx_img.drawImage(img, 0, 0, 800, 500);
+    var img = new Image();
+
+    var canvas_ceng = document.getElementById('yan_ceng');
+    var ctx_ceng = canvas_ceng.getContext('2d');
+
+    var mX = 0;//移动距离
+    var dX = 0, dX1 = 0; //滑动区域
+    var isDrag = false;
+
+    //显示拼图块
+    ctx_img.clearRect(0,0,800,500);
+    ctx_ceng.clearRect(0,0,800,500);
+
+    $('.yan_ceng').css('display', 'block');
+
+    var btn = $('.yan_drag_btn');
+    var mover = $('.yan_drag');
+
+    //生成图
+    getData();
+
+    //按钮点击事件
+    btn.on('mousedown', function (e) {
+        if (isMatch) return;
+        dX = e.pageX;
+        dX1 = e.pageX + 650;
+        btn.html('<->');
+        isDrag = true;
+    });
+
+    //鼠标移动事件
+    mover.on('mousemove', function (e) {
+        if (isDrag) {
+            var x = e.pageX;
+            if (x >= dX && x <= dX1) {
+                mX = e.pageX - dX;
+            }
+            if (x>=dX1) {
+                isDrag = false;
+                mX = 0;
+            }
+            btn.css('left', mX);
+            $('.moved').css('width', mX);
+            ctx_ceng.clearRect(0,0,800,500);
+            ctx_ceng.lineWidth = 2;
+            ctx_ceng.strokeStyle = '#ffffff';
+
+            ctx_ceng.putImageData(imgData, mX,cY-r);
+            ctx_ceng.globalCompositeOperation="destination-in";
+
+            ctx_ceng.save();
+            ctx_ceng.beginPath();
+            ctx_ceng.moveTo(mX, cY);
+            ctx_ceng.lineTo(mX+s/2-r,cY);
+            strockArc(ctx_ceng,mX,n);
+            ctx_ceng.closePath();
+            ctx_ceng.fillStyle = 'green';
+            ctx_ceng.fill();
+            ctx_ceng.clip();
+
+            ctx_ceng.restore();
+        }
+    });
+
+    $(document).on('mouseup', function (e) {
+        isDrag = false;
+        btn.html('|||');
+        if (mX+1 <= cX+3 && mX+1 >= cX-3){
+            testData(resultData);
+            if (resultData.flag) {
+                isMatch = true;
+                $('.exm').css({'background-color': 'transparent', color: 'transparent'});
+                $('.moved').html('验证通过');
+                $('.yan_drag_text').html('');
+            } else {
+                $('.exm').css({'background-color': 'red', color: '#fff'});
+                setTimeout(function () {
+                    $('.exm').css({'background-color': 'transparent', color: 'transparent'});
+                },3000);
+                getData()
+            }
+        } else {
+            $('.exm').css({'background-color': 'red', color: '#fff'});
+            setTimeout(function () {
+                $('.exm').css({'background-color': 'transparent', color: 'transparent'});
+            },3000);
+            getData();
+        }
+    });
+    function testData(data) {
+        $.post('test_data',{ test_time: data.time, test_number: data.number }, function (result) {
+            data.flag = result.result;
+        });
+    }
+    function getData() {
+        //移动距离置为零
+        mX = 0;
+        btn.css('left',0);
+        $('.moved').css('width',0);
+
+        ctx_img.clearRect(0,0,800,500);
+        //获取x,y轴坐标及背景图src
+        $.get('data',function (result) {
+            resultData = result;
+            cX = result.x;
+            cY =result.y;
+            img.src = `img/${result.imgSrc}`;
+        });
+        img.onload = function () {
+            ctx_img.drawImage(img, 0,0,800,500);
+            setTimeout(function () {
+                //背景图
+                // ctx_img.globalCompositeOperation="destination-over";
+
+                //右方拼图块
+                ctx_img.save();
+                ctx_img.lineWidth = 2;
+                ctx_img.strokeStyle = '#ffffff';
+                ctx_img.beginPath();
+                ctx_img.moveTo(cX, cY);
+                ctx_img.lineTo(cX+s/2-r,cY);
+                n = strockArc(ctx_img, cX);
+                ctx_img.closePath();
+                ctx_img.stroke();
+                ctx_img.clip();
+                imgData = ctx_img.getImageData(cX,cY-r,s+r+1,s+2*r+1);
+                ctx_img.fillStyle = 'rgba(255,255,255,0.5)';
+                ctx_img.fillRect(0,0,800,500);
+
+                ctx_img.restore();
+                ctx_img.fillStyle = 'rgba(255,255,255,0.0)';
+                ctx_img.fillRect(0,0,800,500);
+
+                //左方拼图块
+                ctx_ceng.clearRect(0,0,800,500);
+                ctx_ceng.lineWidth = 2;
+                ctx_ceng.strokeStyle = '#ffffff';
+
+                ctx_ceng.putImageData(imgData, mX+1,cY-r);
+                ctx_ceng.globalCompositeOperation="destination-in";
+                ctx_ceng.save();
+                ctx_ceng.beginPath();
+                ctx_ceng.moveTo(mX, cY);
+                ctx_ceng.lineTo(mX+s/2-r,cY);
+                strockArc(ctx_ceng,mX+1,n);
+                ctx_ceng.closePath();
+                ctx_ceng.fillStyle = 'green';
+                ctx_ceng.fill();
+                ctx_ceng.clip();
+
+                ctx_ceng.restore();
+            })
+        };
+
+    }
+});
